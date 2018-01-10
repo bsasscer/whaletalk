fedf6da562f0348982a4973e4484de0b97befbbc
bsasscer <smithbi.maricela@gmail.com>
2018-01-09 16:24:29 -0800

    Add html to index and add script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+var input = "The term hiccup is an example of onomatopoeia";
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+var resultArray = [];
+
+for (var i = 0; i < input.length; i++) {
+    for (var j = 0; j < vowels.length; j++) {
+        if (input[i] === vowels[j]) {
+            resultArray.push(input[i]);
+        }
+    }
+
+    if (input[i] === 'e' || input[i] === 'u') {
+        resultArray.push(input[i]);
+    }
+}
+
+console.log(resultArray.join('').toUpperCase());
