0ebc66a2c09bf4b5270e2b91d2ac8738b0ff9424
bsasscer <smithbi.maricela@gmail.com>
2018-02-02 13:59:59 -0800

    small bug fix to console.log user input

--- script.js
+++ script.js
@@ -2,24 +2,28 @@
 //translate the sentence
 //show the result
 
-var userInput = document.getElementById('textToTranslate').value;
+var userInput = document.querySelector('input');
 var vowels = ['a', 'e', 'i', 'o', 'u'];
 var resultArray = [ ];
 
-document.getElementById('submitSentence').addEventListener('click', translateSentence);
+document.querySelector('form.main-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    console.log(userInput.value);
+});
 
-function translateSentence() {
-    for (var i = 0; i < userInput.length; i++) {
-        for (var j = 0; j < vowels.length; j++) {
-            if (userInput[i] === vowels[j]) {
-                resultArray.push(userInput[i]);
-            }
-        }
 
-        if (userInput[i] === 'e' || userInput[i] === 'u') {
-            resultArray.push(userInput[i]);
-        }
-    }
-
-    console.log(resultArray.join('').toUpperCase());
-};
+// function translateSentence() {
+//     for (var i = 0; i < userInput.length; i++) {
+//         for (var j = 0; j < vowels.length; j++) {
+//             if (userInput[i] === vowels[j]) {
+//                 resultArray.push(userInput[i]);
+//             }
+//         }
+//
+//         if (userInput[i] === 'e' || userInput[i] === 'u') {
+//             resultArray.push(userInput[i]);
+//         }
+//     }
+//
+//     console.log(resultArray.join('').toUpperCase());
+// };
